71fe59e6a65a2651d1272a32ff1e4ceca6c21633
p0l0 <1452110+p0l0@users.noreply.github.com>
2024-02-23 19:10:14 +0100

    Added additional select config parameters

--- custom_components/gruenbeck_cloud/select.py
+++ custom_components/gruenbeck_cloud/select.py
@@ -5,11 +5,18 @@ from collections.abc import Callable
 from dataclasses import dataclass
 import logging
 
-from pygruenbeck_cloud.const import OPERATION_MODES
+from pygruenbeck_cloud.const import (
+    PARAMETER_LANGUAGES,
+    PARAMETER_LED_MODES,
+    PARAMETER_OPERATION_MODES,
+    PARAMETER_REGENERATION_MODES,
+    PARAMETER_WATER_UNITS,
+)
 from pygruenbeck_cloud.models import Device
 
 from homeassistant import config_entries
 from homeassistant.components.select import SelectEntity, SelectEntityDescription
+from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -32,13 +39,165 @@ class GruenbeckCloudEntityDescription(SelectEntityDescription):
 
 SELECTS: tuple[GruenbeckCloudEntityDescription, ...] = (
     GruenbeckCloudEntityDescription(
+        key="regeneration_mode",
+        translation_key="regeneration_mode",
+        options=list(PARAMETER_REGENERATION_MODES.values()),
+        entity_category=EntityCategory.CONFIG,
+        value_fn=lambda device: PARAMETER_REGENERATION_MODES[
+            device.parameters.regeneration_mode  # type: ignore[index]
+        ],
+        update_fn=lambda device, option: {
+            "regeneration_mode": list(PARAMETER_REGENERATION_MODES.keys())[
+                list(PARAMETER_REGENERATION_MODES.values()).index(option)
+            ]
+        },
+    ),
+    GruenbeckCloudEntityDescription(
+        key="water_hardness_unit",
+        translation_key="water_hardness_unit",
+        options=list(PARAMETER_WATER_UNITS.values()),
+        entity_category=EntityCategory.CONFIG,
+        value_fn=lambda device: PARAMETER_WATER_UNITS[
+            device.parameters.water_hardness_unit  # type: ignore[index]
+        ],
+        update_fn=lambda device, option: {
+            "water_hardness_unit": list(PARAMETER_WATER_UNITS.keys())[
+                list(PARAMETER_WATER_UNITS.values()).index(option)
+            ]
+        },
+    ),
+    GruenbeckCloudEntityDescription(
+        key="language",
+        translation_key="language",
+        options=list(PARAMETER_LANGUAGES.values()),
+        entity_category=EntityCategory.CONFIG,
+        value_fn=lambda device: PARAMETER_LANGUAGES[device.parameters.language],  # type: ignore[index]  # noqa: E501
+        update_fn=lambda device, option: {
+            "language": list(PARAMETER_LANGUAGES.keys())[
+                list(PARAMETER_LANGUAGES.values()).index(option)
+            ]
+        },
+    ),
+    GruenbeckCloudEntityDescription(
         key="mode",
         translation_key="mode",
-        options=list(OPERATION_MODES.values()),
-        value_fn=lambda device: OPERATION_MODES[device.parameters.mode],  # type: ignore[index]  # noqa: E501
+        options=list(PARAMETER_OPERATION_MODES.values()),
+        value_fn=lambda device: PARAMETER_OPERATION_MODES[device.parameters.mode],  # type: ignore[index]  # noqa: E501
         update_fn=lambda device, option: {
-            "mode": list(OPERATION_MODES.keys())[
-                list(OPERATION_MODES.values()).index(option)
+            "mode": list(PARAMETER_OPERATION_MODES.keys())[
+                list(PARAMETER_OPERATION_MODES.values()).index(option)
+            ]
+        },
+    ),
+    # Disabled Entities
+    GruenbeckCloudEntityDescription(
+        key="led_ring_mode",
+        translation_key="led_ring_mode",
+        options=list(PARAMETER_LED_MODES.values()),
+        entity_category=EntityCategory.CONFIG,
+        entity_registry_enabled_default=False,
+        value_fn=lambda device: PARAMETER_LED_MODES[device.parameters.led_ring_mode],  # type: ignore[index]  # noqa: E501
+        update_fn=lambda device, option: {
+            "led_ring_mode": list(PARAMETER_LED_MODES.keys())[
+                list(PARAMETER_LED_MODES.values()).index(option)
+            ]
+        },
+    ),
+    GruenbeckCloudEntityDescription(
+        key="mode_individual_monday",
+        translation_key="mode_individual_monday",
+        entity_registry_enabled_default=False,
+        options=list(PARAMETER_OPERATION_MODES.values()),
+        value_fn=lambda device: PARAMETER_OPERATION_MODES[
+            device.parameters.mode_individual_monday  # type: ignore[index]
+        ],
+        update_fn=lambda device, option: {
+            "mode_individual_monday": list(PARAMETER_OPERATION_MODES.keys())[
+                list(PARAMETER_OPERATION_MODES.values()).index(option)
+            ]
+        },
+    ),
+    GruenbeckCloudEntityDescription(
+        key="mode_individual_tuesday",
+        translation_key="mode_individual_tuesday",
+        entity_registry_enabled_default=False,
+        options=list(PARAMETER_OPERATION_MODES.values()),
+        value_fn=lambda device: PARAMETER_OPERATION_MODES[
+            device.parameters.mode_individual_tuesday  # type: ignore[index]
+        ],
+        update_fn=lambda device, option: {
+            "mode_individual_tuesday": list(PARAMETER_OPERATION_MODES.keys())[
+                list(PARAMETER_OPERATION_MODES.values()).index(option)
+            ]
+        },
+    ),
+    GruenbeckCloudEntityDescription(
+        key="mode_individual_wednesday",
+        translation_key="mode_individual_wednesday",
+        entity_registry_enabled_default=False,
+        options=list(PARAMETER_OPERATION_MODES.values()),
+        value_fn=lambda device: PARAMETER_OPERATION_MODES[
+            device.parameters.mode_individual_wednesday  # type: ignore[index]
+        ],
+        update_fn=lambda device, option: {
+            "mode_individual_wednesday": list(PARAMETER_OPERATION_MODES.keys())[
+                list(PARAMETER_OPERATION_MODES.values()).index(option)
+            ]
+        },
+    ),
+    GruenbeckCloudEntityDescription(
+        key="mode_individual_thursday",
+        translation_key="mode_individual_thursday",
+        entity_registry_enabled_default=False,
+        options=list(PARAMETER_OPERATION_MODES.values()),
+        value_fn=lambda device: PARAMETER_OPERATION_MODES[
+            device.parameters.mode_individual_thursday  # type: ignore[index]
+        ],
+        update_fn=lambda device, option: {
+            "mode_individual_thursday": list(PARAMETER_OPERATION_MODES.keys())[
+                list(PARAMETER_OPERATION_MODES.values()).index(option)
+            ]
+        },
+    ),
+    GruenbeckCloudEntityDescription(
+        key="mode_individual_friday",
+        translation_key="mode_individual_friday",
+        entity_registry_enabled_default=False,
+        options=list(PARAMETER_OPERATION_MODES.values()),
+        value_fn=lambda device: PARAMETER_OPERATION_MODES[
+            device.parameters.mode_individual_friday  # type: ignore[index]
+        ],
+        update_fn=lambda device, option: {
+            "mode_individual_friday": list(PARAMETER_OPERATION_MODES.keys())[
+                list(PARAMETER_OPERATION_MODES.values()).index(option)
+            ]
+        },
+    ),
+    GruenbeckCloudEntityDescription(
+        key="mode_individual_saturday",
+        translation_key="mode_individual_saturday",
+        entity_registry_enabled_default=False,
+        options=list(PARAMETER_OPERATION_MODES.values()),
+        value_fn=lambda device: PARAMETER_OPERATION_MODES[
+            device.parameters.mode_individual_saturday  # type: ignore[index]
+        ],
+        update_fn=lambda device, option: {
+            "mode_individual_saturday": list(PARAMETER_OPERATION_MODES.keys())[
+                list(PARAMETER_OPERATION_MODES.values()).index(option)
+            ]
+        },
+    ),
+    GruenbeckCloudEntityDescription(
+        key="mode_individual_sunday",
+        translation_key="mode_individual_sunday",
+        entity_registry_enabled_default=False,
+        options=list(PARAMETER_OPERATION_MODES.values()),
+        value_fn=lambda device: PARAMETER_OPERATION_MODES[
+            device.parameters.mode_individual_sunday  # type: ignore[index]
+        ],
+        update_fn=lambda device, option: {
+            "mode_individual_sunday": list(PARAMETER_OPERATION_MODES.keys())[
+                list(PARAMETER_OPERATION_MODES.values()).index(option)
             ]
         },
     ),
